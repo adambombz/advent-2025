ffa13247e719aa5ce22a3619e2e4236558296b1c
adambombz <39539852+adambombz@users.noreply.github.com>
2025-12-01 20:24:11 -0500

    Update day1.py

--- day1.py
+++ day1.py
@@ -3,14 +3,15 @@ pos=50
 pt1=0
 pt2=0
 
+#keep position in 0-99 range
 def keepInRange(x):
     while x<0:
         x+=100
     while x>99:
         x-=100
-    # print("passed:",passed)
     return x
 
+# takes current position and an instruction (ex. R69) and counts how many times it passes 0
 def passZero(cur,inst):
     count=0
     if inst[:1]=='L':
@@ -23,8 +24,8 @@ def passZero(cur,inst):
                 count+=1
     return count
 
+# runs through instrtion list running passZero function and counting the times the dial ends on 0
 for i in f:
-    print(pt2, pos, i)
     if i[:1]=='L':
         new = pos - int(i[1:])
         pt2+=passZero(pos,i)
@@ -35,5 +36,6 @@ for i in f:
         pos = keepInRange(new)
     if pos==0:
         pt1+=1
+
 
 print(pt1,pt1+pt2)
